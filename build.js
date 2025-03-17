455b2b9922bdc0fab328402d6237e158b78c6922
Jun Takeno <take.yapr@gmail.com>
2025-03-17 23:59:45 +0900

    add index.html

--- build.js
+++ build.js
@@ -1,9 +1,24 @@
 const esbuild = require('esbuild');
 
-esbuild.build({
+const buildOptions = {
     entryPoints: ['src/yapr.css'],
+    bundle: true,
+    logLevel: 'info',
+    watch: true,  // Auto-build on file changes
+};
+
+// Formatted version (for development)
+esbuild.build({
+    ...buildOptions,
+    outfile: 'dist/yapr.css',
+    minify: false,
+}).catch(() => process.exit(1));
+
+// Minified version (for production)
+esbuild.build({
+    ...buildOptions,
     outfile: 'dist/yapr.min.css',
     minify: true,
-    bundle: true,
-    logLevel: 'info'
 }).catch(() => process.exit(1));
+
+console.log('👀 Watching for changes in src/...');
